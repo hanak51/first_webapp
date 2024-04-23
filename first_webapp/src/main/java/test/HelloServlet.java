08724f33a7c3b5535c8ad8b7a3801a4f081cdbe0
dir-FY24komachihana <HanaKomachi()@Dirbato-1367x>
2024-04-23 09:02:05 +0900

    firstcommit

--- first_webapp/src/main/java/test/HelloServlet.java
+++ first_webapp/src/main/java/test/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
      */
     public HelloServlet() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
 	/**
@@ -42,7 +42,7 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
